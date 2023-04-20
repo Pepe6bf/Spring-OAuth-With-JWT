be54f5d99c6611ee0647f70bea73c5427ed60c08
pepe6bf <pepe6bf@gmail.com>
2023-04-20 22:32:43 +0900

    Project 재설정

---
+++ spring-social-login-with-jwt/src/main/java/com/study/springsocialloginwithjwt/SpringSocialLoginWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.study.springsocialloginwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialLoginWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialLoginWithJwtApplication.class, args);
+	}
+
+}
